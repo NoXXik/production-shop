332824b22be131d3824f7ff52739789a19eeb819
NoXXik <ildarsharifullin1994@mail.ru>
2023-02-24 19:50:46 +0300

    fix product card

--- client/common/components/ProductList/ProductItem/ProductItem.tsx
+++ client/common/components/ProductList/ProductItem/ProductItem.tsx
@@ -25,7 +25,7 @@ export default function ProductItem(props: { product: IProduct }) {
             <li className="product-item product-wrapper">
                 <div className="product__image">
                     <a className="product__image-link">
-                        <Image src={`http://localhost:5000/productImages/${images[0]}`} fill
+                        <Image src={`http://${process.env.API_HOST}:${process.env.API_PORT}/productImages/${images[0]}`} fill
                                alt="" className="product__image-img"/>
                     </a>
                     <div className="product__image-labels">
