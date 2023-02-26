4d955c7c0703652d0220c919cf48687646748cbc
NoXXik <ildarsharifullin1994@mail.ru>
2023-02-26 12:56:02 +0300

    fix next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: [
-        '45.146.164.174',
-        'smarthome16.ru'
-    ]
-  },
-}
+const nextjsDistDir = join("src", require("./src/next.config.js").distDir);
+const nextjsServer = next({
+    dev: false,
+    conf: {
+        distDir: nextjsDistDir,
+        images: {
+            domains: ['smarthome16.ru'],
+        }
+    }
+});
+// const nextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: [
+//         '45.146.164.174',
+//         'smarthome16.ru'
+//     ]
+//   },
+// }
 
-module.exports = nextConfig
+module.exports = nextjsServer
 module.exports = {
   // ... rest of the configuration.
   output: 'standalone',
