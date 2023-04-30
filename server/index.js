0adc1f9c67953262e9e90fd2080a6a911579dc8a
NoXXik <ildarsharifullin1994@mail.ru>
2023-04-30 19:24:15 +0300

    dfscs

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
-app.use('/express', router)
+app.use('/api', router)
 
 // app.use('*.php',function(request,response,next) {
 //     execPHP.parseFile(request.originalUrl,function(phpResult) {
